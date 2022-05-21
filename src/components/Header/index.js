7b3681e975a986cbbfff3d249ee274a8f2d8cb89
rikapro0128 <stkssc002@gmail.com>
2022-05-21 13:40:56 +0700

    save code

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,5 +1,51 @@
+import styles from './styles.module.scss';
+import clsx from 'clsx';
+import { LogoIcon, SearchIcon, UploadIcon, MessageIcon, NotifyIcon } from '@/components/Icons';
+import { Avatar } from '@/components/Image';
+
+const searchPlaceHolder = 'Tìm kiếm tài khoản và video';
+
+const [first, ...next] = 'Tải lên';
+const upperCharacter = first.toUpperCase() + next.join('');
+
 function Header() {
-  return <h1>Header</h1>;
+  return (
+    <div className={clsx(styles.header)}>
+      <div className={clsx(styles.bridge)}>
+        <div className={clsx(styles.logo)}>
+          {/* logo tiktok */}
+          <LogoIcon />
+        </div>
+        <div className={clsx(styles.search)}>
+          <input type="text" placeholder={searchPlaceHolder} />
+          <span></span>
+          <div>
+            <SearchIcon />
+          </div>
+        </div>
+        <div className={clsx(styles.control)}>
+          <div className={clsx(styles.btnUpload)}>
+            {/* icon upload */}
+            <UploadIcon />
+            <span>{upperCharacter}</span>
+          </div>
+          <div className={clsx(styles.btnMessage)}>
+            <MessageIcon />
+          </div>
+          <div className={clsx(styles.btnNotify)}>
+            <NotifyIcon />
+          </div>
+          <div className={clsx(styles.btnProfile)}>
+            <div
+              style={{
+                backgroundImage: `url(${Avatar})`,
+              }}
+            ></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
---
+++ src/components/Image/index.js
@@ -0,0 +1,1 @@
+export { default as Avatar } from '@/assets/images/avatar.jpg';
